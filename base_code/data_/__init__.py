c328c9da9863b7ad518a1d6c9e46bc6032f037a7
GiJeong_Kim <kkj981814@gmail.com>
2023-04-21 09:10:33 +0000

    base_code edit

--- base_code/data_/__init__.py
+++
@@ -1,6 +0,0 @@
-from .context_data import context_data_load, context_data_split, context_data_loader, context_data_split_vkfold
-from .dl_data import dl_data_load, dl_data_split, dl_data_loader, dl_data_split_vkfold
-from .image_data import image_data_load, image_data_split, image_data_loader, image_data_split_vkfold
-from .text_data import text_data_load, text_data_split, text_data_loader, text_data_split_vkfold
-from .cat_data import *
-from .cat2_data import *
