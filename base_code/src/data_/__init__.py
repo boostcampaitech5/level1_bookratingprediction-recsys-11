082f7ac42fa692e96132899bc4b03da383568dcc
GiJeong_Kim <kkj981814@gmail.com>
2023-04-24 09:30:15 +0000

    Edit data_/__init__.py

--- base_code/src/data_/__init__.py
+++ base_code/src/data_/__init__.py
@@ -3,4 +3,3 @@ from .dl_data import dl_data_load, dl_data_split, dl_data_loader, dl_data_split_
 from .image_data import image_data_load, image_data_split, image_data_loader, image_data_split_vkfold
 from .text_data import text_data_load, text_data_split, text_data_loader, text_data_split_vkfold
 from .cat_data import *
-from .cat2_data import *
